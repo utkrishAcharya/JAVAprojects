71dd575f01acbdb621bf1db48b8028d0e8b7ee24
utkrishAcharya <acharyautkrish14@gmail.com>
2026-02-01 11:52:45 +0545

    Update Multiplication.java

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -1,8 +1,13 @@
 public class Multiplication {
+    
     public static double calculate(double... nums) {
+        
         if (nums.length == 0) return 0;
+        
         double result = 1;
+        
         for (double n : nums) {
+            
             result *= n;
         }
         return result;
