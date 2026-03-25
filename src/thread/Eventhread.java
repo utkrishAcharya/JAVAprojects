3d077921e711fa308d5c8a336b41f53f38abd5f1
utkrishAcharya <acharyautkrish14@gmail.com>
2026-03-25 20:09:10 +0545

    Update Eventhread.java

--- src/thread/Eventhread.java
+++ src/thread/Eventhread.java
@@ -11,7 +11,9 @@ public class Eventhread extends Thread {
     public void run() {
         for (int i = 2; i <= max; i += 2) {
             System.out.println("EvenThread: " + i);
+            
             try {
+                
                 Thread.sleep(1);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
