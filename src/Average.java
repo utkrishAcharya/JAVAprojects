3139723cd4f5e25bb8685b4f0df0e9008298b19b
utkrishAcharya <acharyautkrish14@gmail.com>
2026-04-04 07:51:06 +0545

    Update Average.java

--- src/Average.java
+++ src/Average.java
@@ -2,6 +2,7 @@ public class Average {
     public static double calculate(double[] values) {
         if (values.length == 0) {
             throw new IllegalArgumentException("Array is empty");
+            
         }
         double sum = 0;
         for (double v : values) sum += v;
